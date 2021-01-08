ed6a02ca7f73b44ce0c574b69c6701214e0899f8
NikhilJeikar <nikhiljeikar@gmail.com>
2021-01-08 15:04:47 +0530

    Initial Commit

--- Base.py
+++ Base.py
@@ -9,3 +9,7 @@ IP = input("Enter the server local ip: ")
 PORT = int(input("Enter the port number: "))
 Mount = input(" Enter the Parent directory to store data: ")
 
+Sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+Sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+Sock.bind((IP, Port))
+Sock.listen(100)
